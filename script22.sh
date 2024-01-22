11a256ca96de9b6e62aef106095faf010e0afc06
Alexey Shevchenko <112550016+alexshevvv@users.noreply.github.com>
2024-01-22 16:51:12 +0300

    Add files via upload

---
+++ script22.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#проверяем наличия аргумента
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <input_file>"
+    exit 1
+fi
+
+#задаём имя входного файла
+input_file="$1"
+
+#создаём промежуточный файл с уникальными словами
+uniq_words_file="words.txt"
+cat "$input_file" | tr -s ' ' '\n' | sort -u > uniq_words_file
+
+#создаём файл со статистикой появления уникальных слов
+stat_file="stat.txt"
+cat "$input_file" | tr -s ' ' '\n' | sort | uniq -c | awk '{print $2":"$1}' > stat_file
+
+#удаление промежуточных файлов при завершении
+trap 'rm -f "$uniq_words_file" "$stat_file"' EXIT
+
+echo "Execution completed successfully. Results are saved in $uniq_words_file and $stat_file."
+exit 0
